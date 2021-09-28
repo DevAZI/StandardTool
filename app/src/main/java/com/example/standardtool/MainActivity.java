d4e4c2771e5bc952c0123a5a0ff5535df9448779
DevAZI <80841284+DevAZI@users.noreply.github.com>
2021-09-28 12:06:44 +0900

    버튼 클릭시 색 변경 버튼 클릭시 invisible

--- app/src/main/java/com/example/standardtool/MainActivity.java
+++ app/src/main/java/com/example/standardtool/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.standardtool;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,39 +16,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int one[] = new int [4];
-        one[0] = 10;
-        one[3] = 20;
-
         Button button = findViewById(R.id.button);
+        TextView textView = findViewById(R.id.textView);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view){
+                textView.setTextColor(Color.BLUE);
                 button.setVisibility(View.GONE);
-
-
-      /*  Toast.makeText(getApplicationContext(), "토스트메시지", Toast.LENGTH_SHORT).show();
-                *//*for(int i = 0; i < one.length; i++)
-                {
-                    Toast.makeText(getApplicationContext(),Integer.toString(one[i]), Toast.LENGTH_LONG).show();
-                }*//*
-        for (int str : one) {
-            try {
-                Toast.makeText(getApplicationContext(), String.valueOf(str), Toast.LENGTH_SHORT).show();
-            }
-            catch (Exception e)
-            {
-            e.printStackTrace();
-            }
-        }*/
             }
         });
 
 
 
-    /*    int two[][] = new int[2][3];
-        two[0][0] =100;
-        two[2][3] = 200;*/
 
 
     }
